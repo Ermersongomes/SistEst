cefd06f03977d02428d0b6c07a35dbc875ce329c
Ermersongomes <ermersonwwe@gmail.com>
2025-07-03 09:35:57 -0300

    Bug resolvido

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .forms import RegisterForm
 
 # Create your views here.
+
 def view_cad_usuario(request):
-    return render(request, 'cadusuario.html')
+    form = RegisterForm()
+
+    return render(request, 'cadusuario.html', {'form': form})
